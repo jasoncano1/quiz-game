ea908a4bc10182228e314b539f45151920d2d51c
[Jason Cano] <jasoncano0509@gmail.com>
2023-08-14 11:08:59 -0500

    added pictures and started the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        Q: 'Question 1',
+        A: ['a','b','c','d'],
+        C: 'b'
+    },
+    {
+        Q: 'Question 2',
+        A: ['a','b','c','d'],
+        C: 'b'
+    },
+    {
+        Q: 'Question 3',
+        A: ['a','b','c','d'],
+        C: 'b'
+    },
+    {
+        Q: 'Question 4',
+        A: ['a','b','c','d'],
+        C: 'b'
+    },
+    {
+        Q: 'Question 5',
+        A: ['a','b','c','d'],
+        C: 'b'
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+
+start.onclick = () => console.log('test');
