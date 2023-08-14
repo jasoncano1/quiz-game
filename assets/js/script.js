e2a1261b9f48e5d9bbe663f101f156cd41bb7da0
[Jason Cano] <jasoncano0509@gmail.com>
2023-08-14 11:37:18 -0500

    added decrementing clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,29 @@
+let clockId;
+let timer = 75;
+let qIndex = 0;
 
-start.onclick = () => console.log('test');
+const init = () => {
+    timer--;
+    document.getElementById('time').innerHTML = timer;
+    if (timer < 1) {
+        clearInterval(clockId);
+    };
+    
+};
+
+const handleQuestions = () => {
+    let {Q,A,C} = questions[qIndex];
+
+    main.innerHTML = `<h1>${Q}</h1><div id="answers"></div>`;
+
+    A.forEach((answer) => {
+        answers.innerHTML += `<button class="answerBtn">${answer}</button>`;
+    });
+};
+
+const clock = () => {
+    clockId = setInterval(init, 1000);
+    handleQuestions();
+}
+
+start.onclick = clock;
