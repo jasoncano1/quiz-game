22b9ca906e28df6e858aa988a327e1a2017868a6
[Jason Cano] <jasoncano0509@gmail.com>
2023-08-22 10:55:49 -0500

    set end game, highscore, defined set score function, configured local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,58 @@
 let clockId;
-let timer = 75;
+let timer = 5;
 let qIndex = 0;
+
+const setScore = async () => {
+    let scores = localStorage.scores ? JSON.parse(localStorage.scores) : [];
+    let initials = document.getElementById('initials').value;
+
+    scores.push({ initials, score: timer });
+    scores = scores.sort((a, b) => b.score - a.score);
+    localStorage.scores = JSON.stringify(scores);
+
+    main.innerHTML = `<h1>High Scores</h1><ol id="scores"></ol>`;
+
+    scores.forEach(player => {
+        document.querySelector('ol').innerHTML += `<li>${player.initials} - ${player.score}</li>`;
+    });
+
+
+    main.innerHTML += `<button onclick="clearScores()">Clear Scores</button><button onclick="resetGame()">Play Again</button>`;
+
+};
+
+const endGame = () => {
+    clearInterval(clockId);
+    let { Q } = questions[qIndex];
+    main.innerHTML = `<h1>Game Over</h1><h3>You got ${timer} points!</h3><p>${Q}</p><p>Enter your initials: <input type="text" id="initials" maxlength="3"></p><button onclick="setScore()">Set Score</button>`;
+};
+
 
 const init = () => {
     timer--;
     document.getElementById('time').innerHTML = timer;
-    if (timer < 1) {
-        clearInterval(clockId);
-    };
-    
+    if (timer < 1) endGame();
+};
+
+const handleAnswers = ans => {
+    if (ans != questions[qIndex].C) timer -= 10;
+
+    qIndex++;
+    handleQuestions();
 };
 
 const handleQuestions = () => {
-    let {Q,A,C} = questions[qIndex];
+    if (qIndex >= questions.length) {
+        clearInterval(clockId);
+        return endGame();
+    };
+
+    let { Q, A } = questions[qIndex];
 
     main.innerHTML = `<h1>${Q}</h1><div id="answers"></div>`;
 
     A.forEach((answer) => {
-        answers.innerHTML += `<button class="answerBtn">${answer}</button>`;
+        answers.innerHTML += `<button onclick="handleAnswers('${answer}')">${answer}</button>`;
     });
 };
 
